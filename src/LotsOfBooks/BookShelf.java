e15ac423400166cc1b5afe808e72653d63e3ebd4
73134 <54319703+73134@users.noreply.github.com>
2019-09-27 09:33:45 -0500

    Update BookShelf.java
    added function overload for fillBookShelf, specifying how many books to place in the shelf and assigning it to a random position in the shelf array

--- src/LotsOfBooks/BookShelf.java
+++ src/LotsOfBooks/BookShelf.java
@@ -1,14 +1,17 @@
 package LotsOfBooks;
+
 public class BookShelf {
 //********Class States***********
 
     private Book[][] aBookShelf;
     private final int NUM_OF_SHELVES = 5;
     private final int BOOKS_PER_SHELF = 20;
+
 //********Constructors***********
     public BookShelf(){
         aBookShelf = new Book[NUM_OF_SHELVES][BOOKS_PER_SHELF];
-        fillBookShelf();
+        fillBookShelf(25);
+        System.out.print(getLength(aBookShelf));
     }
 
 //********Class Behaviors********
@@ -30,11 +33,37 @@ public class BookShelf {
 
     //Adds X books to the in random order.
     public void fillBookShelf(int numOfBooks){
-
+        for(int i = 0; i < numOfBooks; i++){
+            int x = (int)(Math.random()*NUM_OF_SHELVES);
+            int y = (int)(Math.random()*BOOKS_PER_SHELF);
+            //in case the spot is already taken, it will try to find a new spot for it
+            if(aBookShelf[x][y] != null){
+                fillBookShelf(1);
+            }
+            else{
+                aBookShelf[x][y] = new Book();
+            }
+            //amount of books in array should now always be equal to numOfBooks
+        }
     }
 
+    //finds how many non null variables are in an array
+    public static int getLength(Book[][] arr){
 
-//********Getters and Setters****
+        int count = 0;
+        for(Book[] a : arr) {
+            for (Book b : a) {
+                if (b != null)
+                    ++count;
+            }
+        }
+        return count;
+    }
 
-
+    //********Getters and Setters****
 }
+
+
+
+
+
